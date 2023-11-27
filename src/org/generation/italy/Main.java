8af95237b333d938010075c1fd716486c9c1cfc8
claudiolore <claudio.lore001@gmail.com>
2023-11-27 16:48:28 +0100

    classe giusta

--- src/org/generation/italy/Main.java
+++ src/org/generation/italy/Main.java
@@ -22,11 +22,23 @@ eroga():   decrementa la quantità disponibile di 1
 carica(int quantità): incrementa la quantità disponibile del valore quantità
 Nella classe distributore modificare l'hashmap bevande in un HashMap<Integer, Bevande>  (chiave: codice della bevanda; valore: tutto l'oggetto bevanda), e modificare la classe Distributore di conseguenza, gestendo anche la quantità disponibile
  */
+	Scanner sc=new Scanner(System.in);	
+	float monetaInserita;
+	String scelta, risposta="";
 	distributoreBevande db1=new distributoreBevande();
 	
-	db1.inserisciMoneta();
-	db1.erogaBevande();
+	
+	do {
+		System.out.println("bevande disponibili");
+		db1.elencaBevande();
+		System.out.println("Inserisci moneta");
+		monetaInserita=Float.parseFloat(sc.nextLine());
+		db1.inserisciMoneta(monetaInserita);
 		
+		
+		
+	}while(risposta.equals("si"));
+
 	}
 
 }
--- src/org/generation/italy/distributoreBevande.java
+++ src/org/generation/italy/distributoreBevande.java
@@ -14,19 +14,14 @@ credito: float che indica il denaro inserito nel distributore
 e i seguenti metodi:  */
 
 public class distributoreBevande {
-	Scanner sc=new Scanner(System.in);
+	
 	private float credito;
 	private HashMap<String,Float> bevande=new HashMap<>();
 	private ArrayList<Float> moneteAccettate= new ArrayList<>(); 
-	float monetaInserita;
-	String scelta, risposta;
+	
 
-	public distributoreBevande(float credito,  HashMap<String, Float> bevande,
-			ArrayList<Float> moneteAccettate) {
-		super();
-		this.credito = credito;
-		this.bevande = bevande;
-		this.moneteAccettate = moneteAccettate;
+	public distributoreBevande() {
+	
 			bevande.put("coca-cola",2.0f);
 			bevande.put("fanta",2.0f);
 			bevande.put("sprite",1.80f);
@@ -38,37 +33,21 @@ public class distributoreBevande {
 			moneteAccettate.add(1.0f);
 			moneteAccettate.add(2.0f);
 			credito=0;
-		}
+	}
 	
-
-
-
-	/*(inserisce una moneta nel ditributore incrementando il credito)*/
-	public void inserisciMoneta ()
+	
+	public void inserisciMoneta (float monetaInserita)
 	{
-		do {
-			System.out.println("Inserisci una moneta (0.10/0.20/0.50/1.0/2.0)");
-			monetaInserita=Float.parseFloat(sc.nextLine());
 			
 			if(moneteAccettate.contains(monetaInserita))
 			{
 				credito+=monetaInserita;
-				System.out.println("Il tuo credito è: "+credito);
 			}
-			else
-			{
-				System.out.println("Moneta non esistente o accettata");
-			}
-			System.out.println("Vuoi inserire una nuova moneta?");
-			risposta=sc.nextLine();
-		}while(risposta.equals("si"));
+			
 	}
 	
 	
-
-	/*(se esiste la bevanda e il credito è sufficiente mostra un messaggio 
-	 "bevanda erogata. Il tuo resto è: ...."*/
-	public void erogaBevande ()
+	public void erogaBevande (String scelta)
 	{
 		if(bevande.containsKey(scelta)&&credito>bevande.get(scelta))
 		{
@@ -81,7 +60,15 @@ public class distributoreBevande {
 		}
 	}
 	
+	public void elencaBevande()
+	{
+		for(String n:bevande.keySet())
+			System.out.println(n + " " + bevande.get(n) + " " +  bevande.get(n) + "€");
+	}
 	
-	
+	public void mostraCredito()
+	{
+		System.out.println("Credito: "+credito);
+	}
 	
 }
